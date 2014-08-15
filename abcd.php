dd0869b28a6e19cfa62720c98d0fc56ad1efbfb7
rangsimant <sunnamaja@hotmail.com>
2014-08-15 17:58:16 +0700

    3 section

--- abcd.php
+++ abcd.php
@@ -1,11 +1,11 @@
 <meta http-equiv="Content-Type" content="test/html;charset=UTF-8">
 <?php
 ini_set('max_execution_time', 0);
-$dateEnd = date("2014-08-9 00:00:00");
-$dateStart = date("2014-08-13 00:00:00");
+$dateEnd = date("2014-08-10 00:00:00");
+$dateStart = date("2014-08-14 23:59:59");
 $x=0;
 $url = "https://graph.facebook.com/ceclip/posts?&until=".strtotime($dateStart)."&access_token=1646024095622253|25ad52f609d75e675d20704f44c3f7cf";
-while (strtotime($dateStart) != strtotime($dateEnd)) {
+while (strtotime($dateStart) >= strtotime($dateEnd)) {
 	getPost($date,$x,$dateEnd,$dateStart,$url); // call use function getPost
 }
 function getPost(& $date,& $x,& $dateEnd,& $dateStart,& $url){
@@ -20,7 +20,7 @@ function getPost(& $date,& $x,& $dateEnd,& $dateStart,& $url){
 				foreach ($data->paging as $key2 => $value2) {
 					if ($key2=="next") {
 						$url = $value2;
-						$dateStart = date("d-m-Y 00:00:00",strtotime($value->created_time));
+						$dateStart = date("d-m-Y H:i:s",strtotime($value->created_time));
 						return array($url, $dateStart);
 					}
 				}
