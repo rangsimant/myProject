70367c843ac4eaddc325a0f372171edbe92d32bc
rangsimant <sunnamaja@hotmail.com>
2014-08-26 16:35:15 +0700

    WR Edit and Backup

---
+++ apiFacebook.php
@@ -0,0 +1,29 @@
+<meta http-equiv="Content-Type" content="test/html;charset=UTF-8">
+<?php
+ini_set('max_execution_time', 0);
+$dateStart = date("2014-08-12 23:00:00");
+$dateEnd = date("2014-08-10 00:00:00");
+$x=0;
+$url = "https://graph.facebook.com/ceclip/posts?&until=".strtotime($dateStart)."&access_token=1646024095622253|25ad52f609d75e675d20704f44c3f7cf";
+while (strtotime($dateStart) >= strtotime($dateEnd)) {
+	getPost($date,$x,$dateEnd,$dateStart,$url); // call use function getPost
+}
+function getPost(& $date,& $x,& $dateEnd,& $dateStart,& $url){
+		$data = json_decode(file_get_contents($url));
+		foreach ($data->data as $key => $value) {
+			if (isset($value->message)) {
+				$x = $x+1;
+				echo "Key : [".$x."] ".date("d-m-Y H:i:s",strtotime($value->created_time))." ID Post : ".$value->id."<br/>";
+				echo $value->message."<br/>";
+			}
+			if ($key == 24) {
+				foreach ($data->paging as $key2 => $value2) {
+					if ($key2=="next") {
+						$url = $value2;
+						$dateStart = date("d-m-Y H:i:s",strtotime($value->created_time));
+						return array($url, $dateStart);
+					}
+				}
+			}
+		}
+}
